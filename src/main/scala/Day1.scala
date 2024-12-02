9411cb4fcb54ee0c72b98325a1ec455b66d51ad6
Sebastian Wiedemann <sebastian.wiedemann@tuwien.ac.at>
2024-12-02 17:56:40 +0100

    project reset and day 1 solution

--- src/main/scala/Day1.scala
+++ src/main/scala/Day1.scala
@@ -1,13 +1,34 @@
-import util.Day;
+import util.Day
+import scala.collection.mutable.ArrayBuffer
+import scala.collection.mutable.HashMap
 
-object Day1 extends Day(1):
-  override def solve(): Unit =
-    val (l, r) = inputLines.map(_.split(" {3}")).map(r => (r(0).toInt, r(1).toInt)).unzip
+object Day1 extends Day(1) {
+  override def solve(): Unit = {
 
-    //Part 1
-    println(l.sorted.zip(r.sorted).map((l, r) => Math.abs(l - r)).sum)
+    val leftList =  ArrayBuffer[Int]()
+    val rightList =  ArrayBuffer[Int]()
 
-    val counts = r.map(i => i -> r.count(_ == i)).toMap.withDefaultValue(0)
+    val scoreMap = HashMap[Int, Int]()
 
-    //Part 2
-    println(l.map(i => i * counts(i)).sum)
+    inputLines.foreach{ line =>
+      val parts = line.split("   ")
+      val (l,r) = (parts(0).toInt, parts(1).toInt)
+
+      leftList.insert(leftList.lastIndexWhere(_ < l)+1, l)
+      rightList.insert(rightList.lastIndexWhere(_ < r)+1, r)
+    }
+    
+    // Part 1
+    println(leftList.zip(rightList).map { case (a,b) => (a-b).abs}.sum)
+
+    rightList.foreach{elem => 
+      scoreMap.updateWith(elem) {
+        case Some(x) => Some(x+1)
+        case None => Some(1)
+      }
+    }
+
+    // Part 2
+    println(leftList.map{x => x*scoreMap.getOrElse(x, 0) }.sum)
+  }
+}
